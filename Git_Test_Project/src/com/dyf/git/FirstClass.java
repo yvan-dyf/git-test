78836890a313723792f332bbe232b49d0fbe8722
yvan <626790194@qq.com>
2019-08-20 15:42:57 +0800

    把本地工程添加到git

---
+++ Git_Test_Project/src/com/dyf/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.dyf.git;
+
+/**
+ * @author yvan
+ * @date 2019/8/20 - 15:19
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("Hello git!!");
+    }
+}
